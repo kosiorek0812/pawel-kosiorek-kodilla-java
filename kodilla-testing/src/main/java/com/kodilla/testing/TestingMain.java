63eef960f367b5bdddc921f1fa903f15345e415a
Paweł Kosiorek <48069213+PawelKosiorek@users.noreply.github.com>
2024-10-15 15:21:08 +0200

    Add test for calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.testing;
 import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
@@ -11,5 +12,21 @@ public class TestingMain {
         } else {
             System.out.println("Test FAIL");
         }
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(10, 20);
+        if (addResult == 30) {
+            System.out.println("Test PASSED");
+        } else {
+            System.out.println("Test FAILED");
+        }
+        int subtractResult = calculator.subtract(20, 10);
+
+        if (subtractResult == 10) {
+            System.out.println("Test PASSED");
+        } else {
+            System.out.println("Test FAILED");
+        }
+
     }
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,13 +1,7 @@
 package com.kodilla.testing.calculator;
 
 public class Calculator {
-    private int a;
-    private int b;
 
-    public Calculator(int a, int b) {
-        this.a = a;
-        this.b = b;
-    }
     public int add(int a, int b) {
         return a + b;
     }
